73f0b605e93c0534cc2c144aca4164463c1dead3
Rasulov Baxtzod <141494256+Baxtzodjon@users.noreply.github.com>
2023-08-12 11:38:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/* вчерашний домашние задания */
+
+console.log('aaa@bbb@ccc'.replace('@', '@'));
+console.log('aaa@bbb@ccc'.replaceAll('@', '!'));
+
+
+console.log('HELLOWORLD'.slice(0));
+console.log('Hello world'.slice(0).trim());
+
+
+console.log('Hello, it is HTML'.slice(0));
+console.log('Hello it is not JS'.slice(0).trim());
+
+
+`let a = 'alex'`
+
+let a = 'alex'
+console.log(a);
+
+
+let math = Math.random()
+console.log(math);
+
+
+let Alex  = 'Lex Luter has a big suit'
+console.log(Alex);
+
+Alex = 'Alex'
+console.log(Alex);
